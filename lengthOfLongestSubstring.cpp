0a8a28e57a8147a231a639c83107be6ff3b82c39
Shubham Attri <shubham21354@iiitd.ac.in>
2023-07-09 18:03:22 +0530

    lengthOfLongestSubstring.cpp

---
+++ lengthOfLongestSubstring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> ch;
+
+        int i =0;
+        int j =0;
+
+        int result = 0;
+
+
+        while(j<s.size()){
+            if(ch.find(s[j]) == ch.end()){
+                ch.insert(s[j]);
+                result = max(result, j - i +1);
+                j++;
+            }
+            else{
+                ch.erase(s[i]);
+                i++;
+            }
+
+
+        }
+
+        return result;
+
+    }
+};
