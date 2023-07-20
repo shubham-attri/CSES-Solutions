23f20739cb8d95777de0d3737a1dfe16096b04ea
Shubham Attri <shubham21354@iiitd.ac.in>
2023-07-21 01:48:49 +0530

    added cses directory

---
+++ CSES/two sets.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Shubham Attri on 20/07/23.
+//
+
+#include "template.h"
+//
+// Created by Shubham Attri on 20/07/23.
+//
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+#define int long long
+#define vch vector<char>
+#define vi vector<int>
+#define vst vector<string>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define si set<int>
+#define mii map<int,int>
+#define vinp(a,n) for(int i=0;i<n;i++)cin>>a[i];
+#define vout(a,n) for(int i=0;i<n;i++)cout a[i]<<" ";
+#define fo(n) for(int i =0 ; i < n ; i++)
+#define en <<endl;
+#define cout cout<<
+
+
+
+#ifndef INTERNPREP_TEMPLATE_H
+#define INTERNPREP_TEMPLATE_H
+
+#endif //INTERNPREP_TEMPLATE_H
+
+
+
+
+int32_t main(){
+
+    int n;
+    cin>>n;
+
+    if((n*(n+1)/2)%2 != 0 )
+        cout "NO";
+    else{
+
+    }
+
+
+    return 0;
+}
