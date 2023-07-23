8aa3c1dbd331ab063d5934fb83e82ddb1cb3dd59
Shubham Attri <shubham21354@iiitd.ac.in>
2023-07-24 03:03:08 +0530

    gray code.cpp

---
+++ CSES/gray code.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Shubham Attri on 23/07/23.
+//
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+#define int long long
+#define vch vector<char>
+#define vi vector<int>
+#define vst vector<string>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define si set<int>
+#define mii map<int,int>
+#define vinp(a,n) for(int i=0;i<n;i++)cin>>a[i];
+#define vout(a,n) for(int i=0;i<n;i++)cout a[i]<<" ";
+#define fo(n) for(int i =0 ; i < n ; i++)
+#define en <<endl;
+#define cout cout<<
+#define mod 1e9 +7
+
+
+
+#ifndef INTERNPREP_TEMPLATE_H
+#define INTERNPREP_TEMPLATE_H
+
+#endif //INTERNPREP_TEMPLATE_H
+
+
+
+
+int32_t main(){
+
+    int n;
+    cin>>n;
+
+
+
+
+    return 0;
+}
