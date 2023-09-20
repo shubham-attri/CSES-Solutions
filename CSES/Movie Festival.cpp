83a5722cf1ef1c4956745256b45bf8d041cc62d7
Shubham Attri <shubham21354@iiitd.ac.in>
2023-09-20 15:50:25 +0530

    Add Movie Festival problem solution
    Added a new file solving the Movie Festival problem from CSES. The solution sorts the movie start and end times using vector pairs, then iterates over this vector to check the maximum number of movies one can attend. The vector push_back and sort functions were used to ensure efficient handling of the input data. This approach ensures the solution in less time complexity, complying with the CSES problem constraints.

---
+++ CSES/Movie Festival.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+
+using namespace std;
+
+#define int long long
+#define vch vector<char>
+#define vi vector<int>
+#define vst vector<string>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define si set<int>
+#define mii map<int,int>
+#define vinp(a,n) for(int i=0;i<n;i++)cin>>a[i];
+#define vout(a,n) for(int i=0;i<n;i++)cout a[i]<<" ";
+#define fo(i,n) for(int i =0 ; i < n ; i++)
+#define en <<endl;
+#define cout cout<<
+#define mod 1e9 +7
+#define sort(a) sort(a.begin(),a.end())
+
+
+
+#ifndef INTERNPREP_TEMPLATE_H
+#define INTERNPREP_TEMPLATE_H
+
+#endif //INTERNPREP_TEMPLATE_H
+
+
+int32_t main() {
+    int n,start,end,ce =0,ans=0;
+    cin>>n;
+
+    vector<pair<int,int>> time;
+    while(n--)
+    {
+        cin>>start>>end;
+        time.pb(mp(end,start));
+    }
+
+    sort(time);
+
+    for(auto element: time){
+        if(element.second >= ce)
+        {
+            ce = element.first;
+            ans++;
+        }
+    }
+
+    cout ans en
+
+    return 0;
+}
