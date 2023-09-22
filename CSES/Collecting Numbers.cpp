8f4e58d6b891a0b2570fe700a647644377ca3ca5
Shubham Attri <shubham21354@iiitd.ac.in>
2023-09-22 20:51:34 +0530

    Collecting Numbers.cpp

---
+++ CSES/Collecting Numbers.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+
+using namespace std;
+
+#define int long long
+#define vch vector<char>
+#define vi vector<int>
+#define vst vector<string>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define si set<int>
+#define mii map<int,int>
+#define vinp(a,n) for(int i=0;i<n;i++)cin>>a[i];
+#define vout(a,n) for(int i=0;i<n;i++)cout a[i]<<" ";
+#define fo(i,n) for(int i =0 ; i < n ; i++)
+#define en <<endl;
+#define cout cout<<
+#define mod 1e9 +7
+#define sort(a) sort(a.begin(),a.end())
+
+
+
+#ifndef INTERNPREP_TEMPLATE_H
+#define INTERNPREP_TEMPLATE_H
+
+#endif //INTERNPREP_TEMPLATE_H
+
+
+int32_t main() {
+    int n;cin>>n;
+    vi a(n); vi ia(n+1);
+    ia[0] = 0;int count =1;
+    fo(i,n){
+        int x;cin>>x;ia[x]=i;
+    }
+    for(int i =1;i<n+1;i++){
+        if(ia[i] < ia[i-1]) count++;
+    }
+    cout count en
+    return 0;
+}
